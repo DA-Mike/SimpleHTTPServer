a6b334fae76962e9070c8dda86dfe244b0468cea
Mike Rugh <mike.rugh@gmail.com>
2022-12-22 18:51:19 -0800

    adds server

---
+++ src/main/java/com/SimpleHTTPServer/SimpleHTTPServer.java
@@ -0,0 +1,36 @@
+package com.SimpleHTTPServer;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.IOException;
+import java.util.Date;
+
+public class SimpleHTTPServer {
+
+    public static void main(String[] args) throws Exception {
+        final ServerSocket server = new ServerSocket(8080);
+        System.out.println("Listening for connection on port 8080 ....");
+//        while (true){
+//            final Socket clientSocket = server.accept();
+//            InputStreamReader inputStreamReader
+//            = new InputStreamReader(clientSocket.getInputStream());
+//            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+//            String line = bufferedReader.readLine();
+//            while (!line.isEmpty() && line != null) {
+//                System.out.println(line);
+//                line = bufferedReader.readLine();
+//            }
+//        }
+        while (true) {
+            try (Socket socket = server.accept()) {
+                Date today = new Date();
+                String httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + today;
+                socket.getOutputStream()
+                        .write((httpResponse.getBytes("UTF-8")));
+            }
+        }
+    }
+
+}
